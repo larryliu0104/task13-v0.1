2ce6a8de87a15d0c2245feafa42e4307dc064f03
larryliu0104 <larryliu0104@hotmail.com>
2016-05-04 23:07:49 -0400

    task 13 framework
    task 13 framework

---
+++ src/controller/Controller.java
@@ -0,0 +1,120 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.genericdao.DAOException;
+import org.genericdao.RollbackException;
+import org.genericdao.Transaction;
+
+import model.Model;
+
+//import controller.customer.ChangeCustomerPasswordAction;
+//import controller.customer.CustomerBuyFundAction;
+
+import util.Log;
+
+public class Controller extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1561542386870494042L;
+	private static final String TAG = "Controller";
+
+	public void init() throws ServletException {
+
+		Model model;
+		try {
+			model = new Model(getServletConfig());
+
+//			try {
+//				if (model.getEmployeeDAO().getCount() == 0) {
+//					createAdminAccount(model);
+//				}
+//			} catch (RollbackException e) {
+//				e.printStackTrace();
+//			} finally {
+//				if (Transaction.isActive()) {
+//					Transaction.rollback();
+//				}
+//			}
+
+
+			Action.add(new HelloWorldAction(model));
+//			Action.add(new CreateCustomerAccountAction(model));
+//			Action.add(new CreateEmployeeAccountAction(model));
+//			Action.add(new CreateFundAction(model));
+//			Action.add(new SearchCustomerAction(model));
+//			Action.add(new DepositCheckAction(model));
+//			Action.add(new ViewCustomerTransactionHistoryAction(model));
+//			Action.add(new EmployeeViewCustomerProfileAction(model));
+//			Action.add(new ViewAllHistoryAction(model));
+
+		} catch (DAOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String servletPath = request.getServletPath();
+		String action = getActionName(servletPath);
+		Log.i("perform action ", action);
+
+		HttpSession session = request.getSession(true);
+		String nextPage = Action.perform(action, request);
+		Log.i("next page ", nextPage);
+		sendToNextPage(nextPage, request, response);
+
+	}
+
+	private void sendToNextPage(String nextPage, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		if (nextPage == null) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND, request.getServletPath());
+			return;
+		}
+		if (nextPage.endsWith(".do")) {
+			response.sendRedirect(nextPage);
+			return;
+		}
+		if (nextPage.endsWith(".jsp")) {
+			RequestDispatcher d = request.getRequestDispatcher("WEB-INF/" + nextPage);
+			d.forward(request, response);
+			return;
+		}
+		if (nextPage.startsWith("http://")) {
+			response.sendRedirect(nextPage);
+			return;
+		}
+
+		Log.e(TAG, "next page is " + nextPage);
+		// RequestDispatcher d = request.getRequestDispatcher("WEB-INF/" +
+		// nextPage);
+		// d.forward(request, response);
+		throw new ServletException(
+				Controller.class.getName() + ".sendToNextPage(\"" + nextPage + "\"): invalid extension.");
+
+	}
+
+	private String getActionName(String path) {
+		int slash = path.lastIndexOf('/');
+		return path.substring(slash + 1);
+	}
+
+
+
+}
+
---
+++ src/model/Model.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.io.File;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import org.genericdao.ConnectionPool;
+import org.genericdao.DAOException;
+
+public class Model {
+//		private CustomerDAO customerDAO;
+//		private EmployeeDAO employeeDAO;
+	    private HelloWorldDAO helloWorldDAO;
+
+		private static final String TAG = "Model";
+
+		public Model(ServletConfig config) throws ServletException, DAOException {
+			String jdbcDriver = config.getInitParameter("jdbcDriverName");
+			String jdbcURL = config.getInitParameter("jdbcURL");
+			ConnectionPool pool = null;
+			if ("\\".equals(File.separator)) {
+				pool = new ConnectionPool(jdbcDriver, jdbcURL, "root", "");
+			} else {
+				pool = new ConnectionPool(jdbcDriver, jdbcURL);
+			}
+			helloWorldDAO = new HelloWorldDAO();
+//			customerDAO = new CustomerDAO(pool, "Customer");
+//			employeeDAO = new EmployeeDAO(pool, "Employee");
+
+		}
+}
