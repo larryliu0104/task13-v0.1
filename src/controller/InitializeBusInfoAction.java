b3b57be315ed0051890dfdbf4892d6d38ff2e160
larryliu0104 <larryliu0104@hotmail.com>
2016-05-10 12:59:25 -0400

    update initialzebusinfoaction
    update initialzebusinfoaction

--- src/controller/InitializeBusInfoAction.java
+++ src/controller/InitializeBusInfoAction.java
@@ -1,5 +1,44 @@
 package controller;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class InitializeBusInfoAction {
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.sun.javafx.collections.MappingChange.Map;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+public class InitializeBusInfoAction extends Action {
+	// key stopId, value list of routes
+	public HashMap<String, ArrayList<String>> stopAndRouteMap = new HashMap<>();
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "init.do";
+	}
+
+	@Override
+	public String perform(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		if (stopAndRouteMap.size() == 0) {
+			addStopandRouteInfo();
+		}
+		return "homepage.jsp";
+	}
+	
+	private void addStopandRouteInfo() {
+		JsonArray allAvaiRoutes = getAllAvaiRoutes();
+		
+		for (int i = 0; i < allAvaiRoutes.size(); i++) {
+			JsonElement currRoute = allAvaiRoutes.get(i);
+		}
+	}
+	private JsonArray getAllAvaiRoutes() {
+		return null;
+	}
 
 }
