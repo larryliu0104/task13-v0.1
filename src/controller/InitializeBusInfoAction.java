bbb42e0189510da8367fa8fb3ead7ae95bf78d79
larryliu0104 <larryliu0104@hotmail.com>
2016-05-10 22:54:36 -0400

    update init bus info action
    fix bugs parsing json

--- src/controller/InitializeBusInfoAction.java
+++ src/controller/InitializeBusInfoAction.java
@@ -1,17 +1,23 @@
 package controller;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
-
 
 import DataBean.RouteOfStop;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import util.Log;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSyntaxException;
 
+import util.Log;
 
 public class InitializeBusInfoAction extends Action {
 	// key stopId, value list of routes
@@ -30,24 +36,36 @@ public class InitializeBusInfoAction extends Action {
 		// TODO Auto-generated method stub
 		if (stopAndRouteMap.size() == 0) {
 			Log.e("init", "map is empty");
-			addStopandRouteInfo();
+			try {
+				addStopandRouteInfo();
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return JSP_NAME;
 	}
 
-
-	private void addStopandRouteInfo() {
-		JsonArray allStops = (JsonArray) new JsonParser().parse("all-stops.json");
-		for (JsonElement curr : allStops) {
-			Double lat = curr.getAsJsonObject().getAsJsonPrimitive("lat").getAsDouble();
-			Double lon = curr.getAsJsonObject().getAsJsonPrimitive("lon").getAsDouble();
-			String stopId = curr.getAsJsonObject().getAsJsonPrimitive("stpid").getAsString();
-			String stopName = curr.getAsJsonObject().getAsJsonPrimitive("stpnm").getAsString();
-			JsonArray routes = curr.getAsJsonObject().getAsJsonArray("routes");
-			for (JsonElement currRoute : routes) {
-				String direction = currRoute.getAsJsonObject().getAsJsonPrimitive("dir").getAsString();
-				String routeId = currRoute.getAsJsonObject().getAsJsonPrimitive("rt").getAsString();
-				// signature of RouteOfStop(String stopId, String stopName, String direction, String routeId, double latitude, double longitude)
+	private void addStopandRouteInfo() throws JsonSyntaxException, IOException {
+		FileReader f = new FileReader("E:\\all_stops.json");
+		BufferedReader br = new BufferedReader(f);
+		String currentJSONString = "";
+		while ((currentJSONString = br.readLine()) != null) {
+			JsonArray allStops = (JsonArray) new JsonParser().parse(currentJSONString);
+			for (JsonElement curr : allStops) {
+				Double lat = curr.getAsJsonObject().getAsJsonPrimitive("lat").getAsDouble();
+				Double lon = curr.getAsJsonObject().getAsJsonPrimitive("lon").getAsDouble();
+				String stopId = curr.getAsJsonObject().getAsJsonPrimitive("stpid").getAsString();
+				String stopName = curr.getAsJsonObject().getAsJsonPrimitive("stpnm").getAsString();
+				Log.i("initInfo", "stopId: " + stopId + " , stopName: " + stopName + " lat: " + lat + " lon: " + lon);
+				String direction = curr.getAsJsonObject().getAsJsonPrimitive("dir").getAsString();
+				String routeId = curr.getAsJsonObject().getAsJsonPrimitive("rt").getAsString();
+				// signature of RouteOfStop(String stopId, String stopName,
+				// String direction, String routeId, double latitude, double
+				// longitude)
 				RouteOfStop currRouteOfStop = new RouteOfStop(stopId, stopName, direction, routeId, lat, lon);
 				if (stopAndRouteMap.containsKey(stopId)) {
 					ArrayList<RouteOfStop> currRouteOfStopList = stopAndRouteMap.get(stopId);
@@ -62,6 +80,12 @@ public class InitializeBusInfoAction extends Action {
 				}
 			}
 		}
+		br.close();
+		// test buses at fifth and morewood
+		// should print 28x, 58, 71B, 71D
+		ArrayList<RouteOfStop> fifthMorewood = stopAndRouteMap.get("1177");
+		for (RouteOfStop curr : fifthMorewood) {
+			Log.e("fifth morewood: ", curr.getRouteId());
+		}
 	}
-
 }
